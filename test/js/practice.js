064cd0fff5ddc0976b7c1151d0e5836e2d51fc01
Gordon <gordy.w.gilbert@gmail.com>
2017-04-26 13:15:08 -0500

    detailed job description

---
+++ test/js/practice.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Gordon on 4/26/2017.
+ */
+
+/*
+function initiate () {
+
+    var elem = document.getElementById('canvas');
+    var canvas = elem.getContext('2d');
+    var gradient = canvas.createLinearGradient(0, 0, 10, 100);
+    gradient.addColorStop(0.5, '#0088FF');
+    gradient.addColorStop(1, '#ff0000');
+
+    canvas.fillStyle = gradient;
+    //canvas.fillStyle = "#0000FF";
+    //canvas.strokeStyle = "#FF0000";
+    //canvas.strokeRect(100, 100, 120, 120);
+    //canvas.fillRect(110, 110, 100, 100);
+    //canvas.clearRect(130, 130, 30, 30);
+    canvas.fillRect(150, 10, 200, 100);
+    canvas.fillRect(400, 10, 100, 100);
+    canvas.fillRect(400, 150, 100, 100);
+
+}
+
+addEventListener("load", initiate);
+
+*/
